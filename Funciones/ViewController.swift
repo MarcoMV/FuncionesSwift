37a0f1a06d31690d9751167ed7ad4dcc4104e5c7
MarcoMV <markovera93@gmail.com>
2015-05-22 02:59:54 -0500

    Función básica de ejemplo

--- Funciones/ViewController.swift
+++ Funciones/ViewController.swift
@@ -12,14 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+      
+        printConsola()   //Llamamos a la funcion
+    }
+    //Función básica de ejemplo
+    func printConsola(){
+        println("Hola estoy dentro de una función")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 

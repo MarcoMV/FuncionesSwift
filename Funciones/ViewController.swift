a9612ce41e487d30885a5fcd7e1bf46fd0dcb4aa
MarcoMV <markovera93@gmail.com>
2015-05-22 02:44:01 -0500

    Initial Commit

---
+++ Funciones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Funciones
+//
+//  Created by Marco Velázquez on 22/05/15.
+//  Copyright (c) 2015 Marco7vr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
